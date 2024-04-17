ae49f6578e087141dd82e3699385f890ca6d7608
Kirill_Shurkin <90568982+57072222@users.noreply.github.com>
2024-04-17 16:07:18 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?
+echo "Первые строки кода на языке PHP";
+?>
+<?php
+echo "<html><head>"; 
+echo '<meta http-equiv="Content-Type" content="text/html; charset=windows-1251">'; 
+echo "<title>Програмка посложнее</title>"; 
+echo "</head>"; 
+echo "<body>"; 
+echo "<h1>Этот скрипт написал студент группы 446 Шуркин К.А.</h1>"; 
+echo "<p>Язык PHP позволяет генерировать HTML-страницы на лету</p>"; 
+echo "</body>"; 
+echo "</html>"; 
+?>
+</body>
+</html>
